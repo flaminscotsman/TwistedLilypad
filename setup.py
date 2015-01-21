02d226890aa14909d5944ff9f5b44c3318f91ec7
flamin_scotsman <ali@flaminscotsman.co.uk>
2015-01-21 22:19:44 +0000

    Prepare setup.py for distribution to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='twistedlilypad',
-    version='0.1',
+    version='1.0.0',
+
+
+    description='An implementation of the lilypad protocol in python',
+
+    url='https://github.com/flaminscotsman/TwistedLilypad',
+
+    author='Alasdair Scott',
+    author_email='ali@flaminscotsman.co.uk',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    keywords='lilypad minecraft',
+
     packages=[
         'twistedlilypad',
         'twistedlilypad.Packets',
@@ -10,9 +39,6 @@ setup(
         'twistedlilypad.Results',
         'twistedlilypad.Utilities'
     ],
-    url='',
-    license='MIT',
-    author='Alasdair Scott',
-    author_email='ali@flaminscotsman.co.uk',
-    description='Implementation of the lilypad protocol in python'
+
+    install_requires=['twisted']
 )
